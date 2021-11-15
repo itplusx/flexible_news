0c80d2a835461fde6a7e24f1fd88ee7a20ec8be3
Ramón Schlosser <schlosser@itplusx.de>
2021-11-15 13:32:34 +0100

    build(deps): bump flexible-pages constraint to ^2.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-10.4.99',
-            'flexible_pages' => '1.0.0-2.1.99'
+            'flexible_pages' => '2.2.0-2.99.99'
         ],
         'conflicts' => [],
         'suggests' => [],
